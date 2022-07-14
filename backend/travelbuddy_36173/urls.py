032aaa81028733f3a2d00ffab61f610ea7636df2
Crowdbotics <team@crowdbotics.com>
2022-07-14 03:25:16 +0000

    Update admin title

--- backend/travelbuddy_36173/urls.py
+++ backend/travelbuddy_36173/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Travelbuddy"
-admin.site.site_title = "Travelbuddy Admin Portal"
-admin.site.index_title = "Travelbuddy Admin"
+admin.site.site_header = "TravelCare"
+admin.site.site_title = "TravelCare Admin Portal"
+admin.site.index_title = "TravelCare Admin"
 
 # swagger
 api_info = openapi.Info(
